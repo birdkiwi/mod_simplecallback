e0a94847d62a39021978385fbcb8b8e012475dd5
Alexey Toklovich <alexey@toklovich.com>
2015-02-21 18:10:32 +0600

    Initial commit

---
+++ mod_simplecallback/helper.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Helper class for Hello World! module
+ *
+ * @package    Joomla.Tutorials
+ * @subpackage Modules
+ * @link http://docs.joomla.org/J3.x:Creating_a_simple_module/Developing_a_Basic_Module
+ * @license        GNU/GPL, see LICENSE.php
+ * mod_helloworld is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ */
+class modSimpleCallbackHelper
+{
+    /**
+     * Retrieves the hello message
+     *
+     * @param array $params An object containing the module parameters
+     * @access public
+     */
+    public static function getHello( $params )
+    {
+        //return 'Hello, World!';
+        return $params;
+    }
+}
+?>
---
+++ mod_simplecallback/mod_simplecallback.php
@@ -0,0 +1,9 @@
+<?php
+// no direct access
+defined('_JEXEC') or die;
+// Include the syndicate functions only once
+require_once( dirname(__FILE__) . '/helper.php' );
+
+$mod_simplecallback = modSimpleCallbackHelper::getHello($params);
+require( JModuleHelper::getLayoutPath('mod_simplecallback'));
+?>
